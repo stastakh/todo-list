9d4d0caced0afac14c24f032d468aca4513c7000
Stanislav Stakh <stastakh@gmail.com>
2019-02-13 18:44:13 +0200

    Add Lists blocks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 
 import classes from './App.css';
-import NewComp from './components/NewComp/NewComp';
+import ListBuilder from './containers/ListBuilder/ListBuilder';
 
 class App extends Component {
   render() {
     return (
       <div className={classes.App}>
-
+        <ListBuilder />
       </div>
     );
   }
---
+++ src/components/ListsBlock/ListsBlock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import classes from './ListsBlock.css';
+
+const listsBlock = () => (
+    <div className={classes.ListsBlock}>
+
+    </div>
+);
+
+export default listsBlock;
