c9a0530bc80280343800c58a867937312dbb6c2b
Stanislav Stakh <stastakh@gmail.com>
2019-02-13 17:43:23 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import classes from './App.css';
+import NewComp from './components/NewComp/NewComp';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div className={classes.App}>
+
       </div>
     );
   }
